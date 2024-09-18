d2026f8824a0e5248df7e5602ced8354944679c1
felipe.joazeiro <felipe.joazeiro@ictcondor.com.br>
2024-09-18 08:17:55 -0300

    Props e organização dos arquivos

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,12 @@
 import { Component } from "react";
+import './styles.css'
 
 export class Button extends Component {
 
     render(){
-        const {text, onClick} = this.props
+        const {text, onClick, disabled} = this.props
         return(
-            <button onClick={onClick}>{text}</button>
+            <button disabled={disabled} onClick={onClick} className="button">{text} </button>
         );
 
     }
--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,8 +1,6 @@
+import './styles.css'
+
 export const PostCard = ({post}) => {
-    
-
-
-
     return (
         <div className='post'>
             <img src={post.cover} alt={post.title} />
