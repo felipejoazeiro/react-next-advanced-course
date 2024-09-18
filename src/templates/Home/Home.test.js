b1acf7a6c6b0cd90e72784b5d26f6a90ffd0fa74
felipe.joazeiro <felipe.joazeiro@ictcondor.com.br>
2024-09-18 13:48:16 -0300

    Início da criação do ambiente de testes

--- src/templates/Home/Home.test.js
+++ src/templates/Home/Home.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import Home from '.';
-
-test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<Home />', ()=>{
+  
+  if('dummy test', ()=>{
+    expect(1).toBe(1);
+  });
+})
